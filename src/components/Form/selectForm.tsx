ef73f9b887c90aa645da817e4849142c4b059300
Ricardo <ricardo_fernandes21@hotmail.com>
2024-04-10 13:34:23 +0200

    Fix- selectform

--- src/components/Form/selectForm.tsx
+++ src/components/Form/selectForm.tsx
@@ -7,20 +7,18 @@ interface DropdownProps {
     dropdownLabel: string;
     options: { id: number; name: string; }[];
   }
-
-
 function classNames(...classes : string []) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Dropdown({dropdownLabel , options } : DropdownProps) {
-  const [selected, setSelected] = useState(options[3])
+  const [selected, setSelected] = useState(options[0])
 
   return (
     <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">{dropdownLabel}</Listbox.Label>
+          <Listbox.Label className="block text-base font-regular leading-6 text-gray-900 text-left">{dropdownLabel}</Listbox.Label>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
