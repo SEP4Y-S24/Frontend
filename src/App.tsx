131cc3731fb4fd9b15f16d6b4a854635bc507ceb
AnnaAndrlova <andrlovaanicka@seznam.cz>
2024-04-10 14:15:46 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import TextArea from "./components/Form/TextArea";
-import "./App.css";
+
 import Button from "./components/Elements/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          <TextArea id="message" rows={5} />
-        </p>
-       
-        
-      
-        
-        
-    
       </header>
-      <Button text="Click me!"  style={"primaryColor"} onClick={() => console.log("Button clicked")} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <TextArea id="message" rows={5} />
+      <Button text="Click me!" color="primaryColor" onClick={() => console.log("Button clicked")} />
     </div>
   );
 }
