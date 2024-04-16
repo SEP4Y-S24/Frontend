a73e7c22e37600d104ed550e60958277b35a5111
Julia <331957@viauc.dk>
2024-04-16 11:03:38 +0200

    Combined components on Messages page

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,6 +1,8 @@
 import Button from "../components/Elements/Button";
 import TextArea from "../components/Form/TextArea";
 import SelectForm from "../components/Form/selectForm";
+import { ContentInnerContainer } from "../components/Layout/ContentInnerContainer";
+import {ContentLayout} from "../components/Layout/ContentLayout";
 
 export const Messages = () => {
 
@@ -19,12 +21,26 @@ export const Messages = () => {
 
     return (
         <>
-            <h1>Messages</h1>
+            <ContentLayout>
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+                <div className="flex-1 h-16 md:h-auto bg-white ">
+                <ContentInnerContainer>
             <TextArea rows={4} labelText="Your message" placeholder="Write your message here" className="mb-4"/>
             <SelectForm dropdownLabel="Select receiver" options={receiverOptions} className="mb-4"/>
             <SelectForm dropdownLabel="Select clocks of receiver" options={clockOptions} className="mb-5"/>
             <Button text="Click me" onClick={() => console.log("Button clicked")} color="bg-primaryColor" hover={"hover:bg-dangerHover"}/>
+            </ContentInnerContainer>
+                </div>
+                <div className="flex-1 md:ml-0 h-16 md:h-auto bg-white">
+                <ContentInnerContainer>
+            <TextArea rows={4} labelText="Your message" placeholder="Write your message here" className="mb-4"/>
+            <SelectForm dropdownLabel="Select receiver" options={receiverOptions} className="mb-4"/>
+            <SelectForm dropdownLabel="Select clocks of receiver" options={clockOptions} className="mb-5"/>
+            <Button text="Click me" onClick={() => console.log("Button clicked")} color="bg-primaryColor" hover={"hover:bg-dangerHover"}/>
+            </ContentInnerContainer>
+                </div>
+            </div>
+            </ContentLayout>
         </>
-
     );
 };
