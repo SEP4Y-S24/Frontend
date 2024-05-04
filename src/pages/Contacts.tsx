14da55eb61449ea51a56b29cc2de0594a465cfe2
Julia <331957@viauc.dk>
2024-05-04 17:45:29 +0200

    add: adding and deleting contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -19,6 +19,11 @@ export const Contacts = () => {
   const [message, setMessage] = useState("");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [email, setEmail] = useState("");
+  const [contacts, setContacts] = useState<
+    Array<{ email: string; imageSrc: string }>
+  >([]);
+
+  const [isHovered, setIsHovered] = useState<number | false>(false);
 
   const handleAddContact = () => {
     setMessage("");
@@ -28,6 +33,15 @@ export const Contacts = () => {
       schema.parse({ email });
       console.log({ email });
       setMessage("Contact successfully added!");
+      //logic to add the contacts to the list
+      setContacts([
+        ...contacts,
+        {
+          email,
+          imageSrc:
+            "https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj",
+        },
+      ]);
       setEmail("");
     } catch (error) {
       if (error instanceof z.ZodError) {
@@ -40,48 +54,16 @@ export const Contacts = () => {
       }
     }
   };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value); // Update email state
+    setEmail(e.target.value);
     setErrors({ ...errors, email: "" });
     setMessage("");
   };
 
-  const handleDeleteContact = () => {
-    // Logic to delete the contact
-  };
-
-  interface ContactsProps {
-    email: string;
-    imageSrc: string;
-  }
-  const Contacts: React.FC<ContactsProps> = ({ email, imageSrc }) => {
-    const [isHovered, setIsHovered] = useState(false);
-
-    return (
-      <div
-        className="flex items-center space-x-3 p-3 hover:bg-whiteHover rounded"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <div className="rounded-full overflow-hidden h-10 w-10 ">
-          <img
-            src={imageSrc}
-            alt={email}
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="flex-grow">
-          <Heading text={email} type={"heading3"} />
-        </div>
-        {isHovered && (
-          <div>
-            <button onClick={handleDeleteContact}>
-              <XMarkIcon className="size-6 text-secondaryText" />
-            </button>
-          </div>
-        )}
-      </div>
-    );
+  const handleDeleteContact = (index: number) => {
+    // Update the contacts state to remove the contact at the given index
+    setContacts((prevContacts) => prevContacts.filter((_, i) => i !== index));
   };
 
   return (
@@ -89,35 +71,48 @@ export const Contacts = () => {
       <ContentLayout className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
         <ContentInnerContainer className="flex-1 h-16 md:h-auto bg-white">
           <Heading text={"Contacts"} type={"heading1"} className="mb-3" />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
-          <Contacts
-            email="example@gmail.com"
-            imageSrc="https://yt3.googleusercontent.com/wzEypbVsmY9BI-IbLwVius4UvC2rejtJB_PTXAdPpYXQ07EIjl5Ms55NCFq_dILwONpxrzE2xA=s900-c-k-c0x00ffffff-no-rj"
-          />
+          {contacts.length === 0 ? (
+            <Heading text={"You have no contacts."} type={"heading4"} />
+          ) : (
+            <>
+              {contacts.map((contact, index) => (
+                <div
+                key={index}
+                className="flex items-center space-x-3 p-3 hover:bg-whiteHover rounded"
+                onMouseEnter={() => setIsHovered(index)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                  <div className="rounded-full overflow-hidden h-10 w-10 ">
+                    <img
+                      src={contact.imageSrc}
+                      alt={contact.email}
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+                  <div className="flex-grow">
+                    <Heading text={contact.email} type={"heading3"} />
+                  </div>
 
-          <PaginationRounded
-            className="flex flex-col items-center"
-            pages={1}
-          ></PaginationRounded>
+                  {/* ensures that the XMark icon appears only on the contact you're hovering over. */}
+                  {isHovered === index && (
+                  <div>
+                    <button onClick={() => handleDeleteContact(index)}>
+                      <XMarkIcon className="size-6 text-secondaryText" />
+                    </button>
+                  </div>
+                )}
+                </div>
+              ))}
+
+              {/* Conditionally render pagination only when there are 5 or more contacts */}
+              {contacts.length >= 8 && (
+                <PaginationRounded
+                  className="flex flex-col items-center pt-4"
+                  pages={2} // Replace with your pagination logic
+                />
+              )}
+            </>
+          )}
         </ContentInnerContainer>
 
         <ContentInnerContainer className="flex-1 h-16 md:h-auto bg-white">
