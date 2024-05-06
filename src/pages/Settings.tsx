f2aa143e4f57882937fd8e910c7d72e88a5982c7
Ricardo <ricardo_fernandes21@hotmail.com>
2024-05-06 17:33:20 +0200

    comented unused variables

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect } from "react";
 import moment from "moment-timezone";
 import SelectForm from "../components/Form/selectForm";
