64080a9d9c001882dd9f153aa08dae1880775fba
AlexandroBolfa <bolfa.alexandro@gmail.com>
2024-05-07 11:31:58 +0200

    implementation of the calendar

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './Dashboard';
 export * from './Messages';
 export * from './Settings';
+export * from './Calendar';
--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,18 +1,17 @@
-
-import { Navigate, Outlet } from 'react-router-dom';
+import {Navigate, Outlet} from 'react-router-dom';
 import {lazyImport} from "../utils/lazyImport";
 import {MainLayout} from "../components/Layout";
 
-
-const { Dashboard } = lazyImport(() => import('../pages/Dashboard'), 'Dashboard');
-const { Messages } = lazyImport(() => import('../pages/Messages'), 'Messages');
-const { Settings } = lazyImport(() => import('../pages/Settings'), 'Settings');
-const { Contacts } = lazyImport(() => import('../pages/Contacts'), 'Contacts');
+const {Dashboard} = lazyImport(() => import('../pages/Dashboard'), 'Dashboard');
+const {Messages} = lazyImport(() => import('../pages/Messages'), 'Messages');
+const {Calendar} = lazyImport(() => import('../pages/Calendar'), 'Calendar');
+const {Settings} = lazyImport(() => import('../pages/Settings'), 'Settings');
+const {Contacts} = lazyImport(() => import('../pages/Contacts'), 'Contacts');
 
 const App = () => {
     return (
         <MainLayout>
-                <Outlet />
+            <Outlet/>
         </MainLayout>
     );
 };
@@ -20,13 +19,14 @@ const App = () => {
 export const protectedRoutes = [
     {
         path: '',
-        element: <App />,
+        element: <App/>,
         children: [
-            { path: '/messages', element: <Messages /> },
-            { path: '/settings', element: <Settings /> },
-            { path: '/contacts', element: <Contacts /> },
-            { path: '', element: <Dashboard /> },
-            { path: '*', element: <Navigate to="." /> },
+            {path: '/messages', element: <Messages/>},
+            {path: '/settings', element: <Settings/>},
+            {path: '/contacts', element: <Contacts/>},
+            {path: '/calendar', element: <Calendar/>},
+            {path: '', element: <Dashboard/>},
+            {path: '*', element: <Navigate to="."/>},
         ],
     },
 ];
