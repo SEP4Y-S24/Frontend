cd7e086b79125c2ca5082551b44879cd409d3a6e
AnnaAndrlova <andrlovaanicka@seznam.cz>
2024-05-09 10:29:21 +0200

    pagination fix

--- src/features/messages/types/index.ts
+++ src/features/messages/types/index.ts
@@ -2,8 +2,13 @@ export interface MessageProps {
     text: string;
     receiver: { id: number; name: string };
     clock: { id: number; name: string };
-  }
-
+}
+export interface SentMessageProps {
+    message: string;
+    receiverId: string;
+    clockId: string;
+    userId: string;
+}
   
 export interface ReceivedMessageProps {
     name: string;
---
+++ src/lib/axios.ts
@@ -0,0 +1,3 @@
+
+export const IP_ADDRESS = "10.154.214.49";
+export const PORT = "8080";
