78c0a10c5e920fec6ee52521b571e5ae788ac363
AnnaAndrlova <andrlovaanicka@seznam.cz>
2024-05-14 12:53:27 +0200

    graph responsive fixes

--- src/features/graph/components/graph.tsx
+++ src/features/graph/components/graph.tsx
@@ -5,8 +5,8 @@ import { GraphProps } from '../types';
 export default function Graph(props : GraphProps) {
   return (
     <div className="flex">
-      <div className="w-full md:w-3/4 lg:w-1/2 xl:w-2/3">
-        <LineChart sx={{width : '50%'}}
+      <div className="w-full">
+        <LineChart
           className="w-full"
           xAxis={[{ data: props.xAxis }]}
           series={[
