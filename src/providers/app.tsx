4ada54df1898e410dfa7099485c9d4c9ecc5f76a
AnnaAndrlova <andrlovaanicka@seznam.cz>
2024-05-23 14:34:33 +0200

    clock setting update

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,5 +1,5 @@
 import Button from "../components/Elements/Button";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {ErrorBoundary} from "react-error-boundary";
 import {
     createBrowserRouter,
@@ -31,13 +31,37 @@ const commonRoutes = [
 ];
 
 export const AppProvider =  () => {
-    //const user =  useUser();
-    const user =  storage.getUser();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [user, setUser] = useState(storage.getUser);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const userData = await storage.getUser();
+                setUser(userData);
+            } catch (error) {
+                console.error('Failed to fetch user data', error);
+                // Handle the error accordingly
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center w-screen h-screen">
+                <span>loading</span>
+            </div>
+        );
+    }
+
     const route = user.data ? protectedRoutes : publicRoutes;
-    //console.log(route);
-    const routes =  [...route, ...commonRoutes];
+    const routes = [...route, ...commonRoutes];
     const router = createBrowserRouter(routes);
+
 
     return (
         <React.Suspense
