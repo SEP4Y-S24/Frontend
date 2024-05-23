af647c0639be1ff1438827ac36f79c8a15387cfd
AnnaAndrlova <andrlovaanicka@seznam.cz>
2024-05-23 15:03:05 +0200

    clock setting update

--- src/features/auth/api/getUser.ts
+++ src/features/auth/api/getUser.ts
@@ -5,7 +5,7 @@ import hardCodedUser from "./hardcodedUser.json";
 // getting user data from the backend by sending JWT token
 //todo: getting user data from the backend by sending JWT token, example https://github.com/alan2207/react-query-auth/blob/master/examples/vite/src/lib/api.ts#L8
     export const getUser = (): Promise<{ user: CreateUserPropsResponse | undefined}> => {
-
+//c
         const hardcodedUser = hardCodedUser as CreateUserPropsResponse;
         if(!Object.keys(hardcodedUser).length) {
             return Promise.reject(new Error('No user data available'));
