bb9b910e38283fd7e827ff844917a981343a7e07
Ricardo <ricardo_fernandes21@hotmail.com>
2024-05-29 12:30:46 +0200

    fix : finished graph feature [ANNA and RICARDO]

--- src/components/Elements/Tab/Tab.tsx
+++ src/components/Elements/Tab/Tab.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, ReactNode, useEffect } from 'react';
 
 interface TabProps {
     label: string;
@@ -7,38 +7,40 @@ interface TabProps {
 
 interface TabsProps {
     children: ReactNode;
-    onTabChange: (label: string) => void;
+    tab : number
+    setTab: React.Dispatch<React.SetStateAction<number>>;
+   
 }
 
-export const Tabs: React.FC<TabsProps> = ({ children, onTabChange }) => {
-    const [activeTab, setActiveTab] = useState(0);
+export const Tabs: React.FC<TabsProps> = ({ children,tab,setTab  }) => {
+    
 
-    const changeTab = (index: number, label: string) => {
-        setActiveTab(index);
-        onTabChange(label); 
-    };
+
+
 
     return (
         <div className="flex flex-col w-full">
             <div className="flex flex-wrap mb-4">
                 {React.Children.map(children, (child, index) => {
-                    const tab = child as React.ReactElement<TabProps>;
+                    const tabr = child as React.ReactElement<TabProps>;
                     return (
                         <button
                             key={index}
                             className={`mr-4 mb-2 px-4 py-2 rounded focus:outline-none ${
-                                activeTab === index
+                                tab === index
                                     ? 'bg-primaryColor text-white'
                                     : 'bg-white text-dark'
                             }`}
-                            onClick={() => changeTab(index, tab.props.label)}
+                            onClick={() =>  setTab(index)
+                                
+                            }
                         >
-                            {tab.props.label}
+                            {tabr.props.label}
                         </button>
                     );
                 })}
             </div>
-            <div>{React.Children.toArray(children)[activeTab]}</div>
+            <div>{React.Children.toArray(children)[tab]}</div>
         </div>
     );
 };
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import Graph from '../features/graph/components/graph';
 import { ContentLayout } from '../components/Layout/ContentLayout';
 import { ContentInnerContainer } from '../components/Layout/ContentInnerContainer';
@@ -8,88 +8,93 @@ import SpinnerComponent from "../features/spinner/SpinnerComponent";
 import { getAvarageMeasurement } from '../features/graph/graphApi';
 import storage from '../utils/storage';
 import {  GraphPropsResponse } from '../features/graph/types';
+import Heading from '../components/Elements/Headings/Heading';
 
 export const Statistics = () => {
     const [data, setData] = useState<GraphPropsResponse>();
     const [graphData,setGraphData] = useState <number[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(true); // Loading state
     const [error, setError] = useState<string>(''); // Error state
-    const [tabComponentValue,setTabComponentValue] = useState<string>("Temperature")
+    const xAxis = [1,2, 4, 6, 8, 10, 12, 14,16,18,20,22,24];
+    const [tab,setTab] = useState<number>(0)
 
     const categoryType = [
-        { id: "1", name: 'Temperature', color: '#13C296' },
-        { id: "2", name: 'Humidity', color: '#FBBF24' },
-        { id: "3", name: 'CO2 Level', color: '#9B51E0' },
-        { id: "4", name: 'Air Condition', color: '#3758F9' }
+        { id: "0", name: 'Temperature', color: '#13C296' },
+        { id: "1", name: 'Humidity', color: '#FBBF24' },
+        { id: "2", name: 'CO2 Level', color: '#9B51E0' },
+        { id: "3", name: 'Air Condition', color: '#3758F9' }
     ];
 
-    const handleData = ()=>{
-        // if(data){
-        //     // const tempData = data.map((item) => {
-        //     //     return item.value
-        //     })
-        //     setGraphData(tempData)
-        }
-
-
-    const fetchData = useCallback(async () => {
-        setIsLoading(true)
-        try {
-          let typeToSend : string
-          switch(tabComponentValue){
-              case "Temperature":
-                  typeToSend = "temperature"
-                  break
-              case "Humidity":
-                  typeToSend = "humidity"
-                  break
-              case "CO2 Level":
-                  typeToSend = "co2" 
-                  break
-              case "Air Condition":
-                  typeToSend = "aircondition"
-                  break 
-              default:
-                  typeToSend = "temperature"      
-          }
-          const response = await getAvarageMeasurement(typeToSend,storage.getClock().clockId)
-          if(response){
-            console.log(response)
-            setData(response)
-            setIsLoading(false)
-          }
-        } catch (error) {
-        setIsLoading(false)
-          setError("Error fetching data, please try again later")
-          console.error("Error : ", error);
-        }
-      },[tabComponentValue]) ;
-
-    const handleTabChange = (label: string) => {
-        setTabComponentValue(label)
-        fetchData();
-        
-    };
     
 
-
     useEffect(() => {
-        setIsLoading(false)
-        fetchData();// DONE CHECK IF IT WORKS
-      }, [fetchData]);
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+              let typeToSend : string
+              switch(tab){
+                  case 0:
+                      typeToSend = "temperature"
+                      break
+                  case 1:
+                      typeToSend = "humidity"
+                      break
+                  case 2:
+                      typeToSend = "co2" 
+                      break
+                  case 3:
+                      typeToSend = "aircondition"
+                      break 
+                  default:
+                      typeToSend = "temperature"      
+              }
+              const response = await getAvarageMeasurement(typeToSend,storage.getClock().clockId)
+              if(response){
+                setData(response)
+                setIsLoading(false)
+              }
+            } catch (error) {
+            setIsLoading(false)
+              setError("Error fetching data, please try again later")
+              console.error("Error : ", error);
+            }
+          }
+        
+        fetchData()
 
-    const xAxis = [1,2, 4, 6, 8, 10, 12, 14,16,18,20,22,24];
+        const handleData = ()=>{
+            if(data){
+                const generateRandomValue = (min:number, max:number) => {
+                    return Math.random() * (max - min) + min;
+                };
+    
+                // Create a mock array with random values between 0 and 50
+                const mockData = xAxis.map(() => generateRandomValue(0, 50));
+    
+                setGraphData(mockData)
+                
+            }
+        }
+        handleData()
+    
+    }, [tab,data])
+
+
+   
+
+    
 
     return (
         <>
             <ContentLayout className="flex flex-row md:flex-row space-y-4 md:space-y-0 md:space-x-4">
                 <ContentInnerContainer className="flex-1 md:h-auto bg-white">
+                    <Heading text='Average measurements for every hour in the last 24 hours' type='heading3' className='mb-6'/>
                     {isLoading && <SpinnerComponent />} {/* Show spinner when loading */}
                     {!isLoading && (
                         <>
                             {error && <p className="text-red-500">{error}</p>}
                             {!error && (
-                                <Tabs  onTabChange={handleTabChange}>
+                                <Tabs tab={tab} setTab={setTab}>
                                     {categoryType.map(type => (
                                         <Tab key={type.id} label={type.name}>
                                             <div className="p-4">
